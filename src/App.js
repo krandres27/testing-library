70a2a365e30d2b2197f979e6a37fd50942fddd78
Andres Gonzalez <krandres27@gmail.com>
2022-04-04 16:14:06 -0500

    testing button that changes the color

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isBlue, setIsBlue] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button style={{ backgroundColor: isBlue ? 'blue' : 'red' }} onClick={() => setIsBlue(true)}>
+        {isBlue ? 'Change to red' : 'Change to blue'}
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color and correct initial text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByRole('button', { name: 'Change to blue' });
+  // screen.debug(buttonElement)
+  expect(buttonElement).toHaveStyle({ backgroundColor: 'red' });
+});
+
+test('button turns blue when clicked and  hast the proper text', () => {
+  render(<App />);
+  const buttonElement = screen.getByRole('button', { name: 'Change to blue' });
+  fireEvent.click(buttonElement);
+  expect(buttonElement).toHaveStyle({ backgroundColor: 'blue' });
+  expect(buttonElement.textContent).toBe('Change to red');
 });
